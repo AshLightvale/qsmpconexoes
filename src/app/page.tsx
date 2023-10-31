40de32ed7820d700ef36d0dbe0b1818a4ba612c0
feyli <48140698+feeeyli@users.noreply.github.com>
2023-10-31 16:21:12 +0000

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+
+import Link from "next/link";
+
+export default function Home() {
+	return (
+		<main className="min-h-screen flex flex-col justify-between items-center py-24">
+			<div className="text-center">
+				<h1 className="text-6xl font-bold">Connections QSMP</h1>
+				<h2 className="text-xl mt-2">Coming soon...</h2>
+			</div>
+			<span>
+				Do you like watch QSMP? Try{" "}
+				<Link
+					href="https://multiqsmp.vercel.app"
+					target="_blank"
+					className="text-[#bfa9e5] p-0 hover:underline"
+				>
+					MultiQSMP
+				</Link>
+			</span>
+		</main>
+	);
+}
