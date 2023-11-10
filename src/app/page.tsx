7f68ca9de61bbe2d043e07815dc1dc53b9c72812
AshLightvale <150376404+AshLightvale@users.noreply.github.com>
2023-11-10 14:27:06 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -176,7 +176,7 @@ export default function Game() {
 									</Button>
 								</div>
 							</div>
-							<div className="bg-muted rounded-md px-6 py-4 text-center flex flex-col items-center gap-10px">
+							<div className="mnb-3 bg-muted rounded-md px-6 py-4 text-center flex flex-col items-center gap-2">
 								<p className="text-balance">
 									Gosta de assistir varios POV do QSMP?
 									Experimente o MultiQSMP:
