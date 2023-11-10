ce56866fcdf8fc3c34ab70f66bc557d5630e1290
AshLightvale <150376404+AshLightvale@users.noreply.github.com>
2023-11-10 00:08:31 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	title: "QSMP Conexões",
+	title: "Conexo QSMP",
 };
 
 export default function RootLayout({
