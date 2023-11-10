9f8bef31ae495fdf6dde5fa5a84847e8d2706d4b
AshLightvale <150376404+AshLightvale@users.noreply.github.com>
2023-11-10 11:29:12 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ module.exports = {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
 				ring: "hsl(var(--ring))",
-				background: "hsl(var(118, 22%, 21%))",
+				background: "hsl(118, 22%, 21%)",
 				foreground: "hsl(var(--foreground))",
 				primary: {
 					DEFAULT: "hsl(--primary)",
